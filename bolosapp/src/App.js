abe60444a69b95c25efd725e55524956d76fc6a3
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-07 18:02:58 +0100

    fixed bug

--- bolosapp/src/App.js
+++ bolosapp/src/App.js
@@ -17,16 +17,43 @@ class App extends Component {
     constructor(props){
         super(props);
         //this.toggle = this.toggle.bind(this);
-        this.state = {
-         
-        }
-                    
       }
 
+      state = {
+        products: [],
+        product: {
+          name: 'sample product',
+          price: 20
+        }
+    }
+             
+
+    componentDidMount(){
+      this.getProducts();
+    }
+
+    getProducts = _ => {
+          fetch('http://localhost:4000/products')
+          .then(response => response.json())
+          .then(response => this.setState({products: response.data }))
+          .catch(err => console.error(err))
+    }
+
+    renderProduct = ({product_id, name}) => <div key={product_id}>{name}</div>
+
+    addProduct = _ => {
+      const { product } = this.state;
+      fetch(`http://localhost:4000/products/add?name=${product.name}&price=${product.price}`)
+      .then(this.getProducts)
+      .catch(err => console.error(err))
+    }
+
   render(){
+    const { products, product  } = this.state;
   return (
     <div>
 
+      
 
 <BrowserRouter>
     <div>
@@ -41,8 +68,14 @@ class App extends Component {
       </Switch>
     </div>
 </BrowserRouter>
-
-
+    <div className="App"> 
+        {products.map(this.renderProduct)}
+        <div>
+          <input value = {product.name} onChange={e => this.setState({product : { ...product , name: e.target.value}})}/>
+          <input value = {product.price} onChange={e => this.setState({product:{ ...product, price: e.target.value}})}></input>
+          <button onClick={this.addProduct}> Add Product </button>
+        </div>
+    </div>
 
 
 
